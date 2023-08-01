3f02974a33f097732a742a489f71b2ecd0938d15
seungrae yu <ns70379@gmail.com>
2023-08-01 15:44:48 +0900

    last

--- src/App.js
+++ src/App.js
@@ -30,6 +30,48 @@ const Main = () => {
   </>)
 }
 
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const users = [
+    {
+      email: 'user1@example.com',
+      password: 'password1'
+    },
+    {
+      email: 'user2@example.com',
+      password: 'password2'
+    }
+    // 더 많은 사용자 정보를 추가할 수 있습니다.
+  ];
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const authenticatedUser = users.find(
+      (user) => user.email === email && user.password === password
+    );
+    if (authenticatedUser) {
+    // 로그인 성공 시 /welcome 페이지로 이동
+    // 로그인 성공 시 localStorage에 로그인 상태 저장
+      localStorage.setItem('isLoggedIn', true);
+      navigate('/');
+    } else {
+      // 로그인 실패 시 처리 (예: 에러 메시지 표시)
+      console.log('Login failed. Please check your email and password.');
+    }
+  };
+
   return (
     <div>
       <div className="login-container">
